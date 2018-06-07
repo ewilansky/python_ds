9ebfed37141a16d252980918249acf7a2fd5e57d
Ethan Wilansky <ewilansky@yahoo.com>
2018-06-06 22:10:21 -0400

    added axis decorations

--- caalms_spider.py
+++ caalms_spider.py
@@ -1,13 +1,14 @@
 import numpy as np
-import pylab as pl
-import matplotlib.pyplot as py
+# import pylab as pl
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
 
 class Radar(object):
 
     def __init__(self, fig, titles, label, rect=None):
         if rect is None:
             # layout area for polar plot - axes postioning left, bottom, width, height in fractions of figure width and height
-            rect = [0.05, 0.15, 0.95, 0.75]
+            rect = [0.05, 0.15, 0.85, 0.75]
 
         self.n = len(titles)
         self.angles = [a if a <=360. else a - 360. for a in np.arange(90, 90+360, 360.0/self.n)]
@@ -23,52 +24,72 @@ class Radar(object):
             ax.grid(False)
             ax.xaxis.set_visible(False)
             self.ax.yaxis.grid(False)
-
+            
         for ax, angle in zip(self.axes, self.angles):
             ax.set_rgrids(range(1, 6), labels=label, angle=angle, fontsize=12)
             # hide outer spine (circle)
             ax.spines["polar"].set_visible(False)
             ax.set_ylim(0, 6)  
             ax.xaxis.grid(True, color='black', linestyle='-')
-            # doesn't do anything...
-            # ax.tick_params(direction='inout', length=20, width=5, color='r', which='both')
-            ticks = ax.xaxis.majorTicks
-            for tick in ticks:
-                tick.tick1On = True
-                tick.tick2On = True
-                # tick.set_visible = True
-                # tick.set_size_inches = 1
-                # tick.inout = True
-               # tick.length=20
-                # tick.color='r'
-                # tick.width=5
-                tick1line = tick.tick1line
-                tick2line = tick.tick2line
-                # tick1line.set_rgrids()
-                tick1line.lineStyles = '-'
-                tick1line.marker = 'o'
-                tick1line.filled_markers = 'o'
-                tick1line.drawStyles = 'steps'
-                tick1line.markevery = 1        
+
+            # draw a line on the y axis at each label
+            ax.tick_params(axis='y', pad=0, left=True, length=6, width=1, direction='inout')
+            # adjust positioning of theta labels
+            ax.tick_params(axis='x', pad=5)     
+            
+    def decorate_ticks(self, axes):
+        for idx, tick in enumerate(axes.xaxis.majorTicks):
+            # print(idx, tick.label._text)
+            # get the gridline
+            gl = tick.gridline
+            gl.set_marker('o')
+            gl.set_markersize(10)
+            if idx == 0:
+                gl.set_markerfacecolor('b')
+            elif idx == 1:
+                gl.set_markerfacecolor('c')
+            elif idx == 2:
+                gl.set_markerfacecolor('g')
+            elif idx == 3:
+                gl.set_markerfacecolor('y')
+            elif idx == 4:
+                gl.set_markerfacecolor('r')
+            elif idx == 5:
+                gl.set_markerfacecolor('black')
+            else: # this doesn't get used. The center doesn't seem to be different than 5
+                gl.set_markerfacecolor('black')
 
     def plot(self, values, *args, **kw):
         angle = np.deg2rad(np.r_[self.angles, self.angles[0]])
         values = np.r_[values, values[0]]
         self.ax.plot(angle, values, *args, **kw)
 
-fig = pl.figure(1)
 
 titles = ['Culture', 'Automation', 'Architecture', 'LEAN', 'Measures', 'Sharing']
 label = list("FMIAB")
+
+# Adding frameon False allows for seeing the edge of the figure.
+fig = plt.figure(figsize=(6.4,4.8), frameon=True)
+
 
 radar = Radar(fig, titles, label)
 radar.plot([3.75, 3.25, 3.0, 2.75, 4.25, 3.5], "-", linewidth=2, color="b", alpha=.7, label="PES")
 radar.plot([3.25, 2.25, 2.25, 2.25, 1.5, 1.75],"-", linewidth=2, color="r", alpha=.7, label="CDS-A")
 
+radar.decorate_ticks(radar.ax)
+
+# this is doing the trick!
+radar.ax.xaxis.grid(clip_on = False)
+
 radar.ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.10),
       fancybox=True, shadow=True, ncol=4)
 
-pl.show()
+# plt.gcf().subplots_adjust(bottom=0.55)
+# plt.tight_layout()
+# plt.rcParams.update({'figure.frameon': False})
+# print(rcParams.keys())
+
+plt.show()
 
 # for saving to file (convenience for inserting into pptx)
 # fig = py.gcf()
